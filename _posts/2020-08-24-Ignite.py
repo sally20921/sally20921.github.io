1f303631cebad4cdef09d802a29bcbe719bdbae6
Seri Lee <sally20921@snu.ac.kr>
2020-08-24 13:26:56 +0900

    Create 2020-08-24-Ignite.py

---
+++ _posts/2020-08-24-Ignite.py
@@ -0,0 +1,18 @@
+---
+layout: post
+title: IGNITE YOUR NETWORKS! 
+categories: Pytorch
+tags: 
+excerpt: 
+---
+
+* `ignite` is a high-level library to help with training and evaluating neural networks in PyTorch flexibly and tansparently.
+* Features
+ * Less code than pure PyTorch while ensuring  maximum control and simplicity
+ * Extensible API for metrics, experiment managers, and other components
+ 
+
+ 
+```python
+tensorboard  --logdir ~/ch3_tb_log --port 6006
+ ```
