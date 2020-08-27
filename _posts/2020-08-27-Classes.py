8018dba85a193d894dd93e6c32b9b2ec682f9708
Seri Lee <sally20921@snu.ac.kr>
2020-08-27 09:05:48 +0900

    Create 2020-08-27-Classes.py

---
+++ _posts/2020-08-27-Classes.py
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Classes
+categories: Python 
+tags: [Python Crash Course]
+excerpt: Python Crash course
+---
+
+* By convention, capitalized names refer to classes in Python. 
+* The  __init__() method 
+```python
+tensorboard  --logdir ~/ch3_tb_log --port 6006
