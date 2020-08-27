1d443c2a4f5aed4216bb5860f32a9e9c03a6b283
Seri Lee <sally20921@snu.ac.kr>
2020-08-27 09:30:24 +0900

    Create 2020-08-27-Decorators.py

---
+++ _posts/2020-08-27-Decorators.py
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Decorators
+categories: Python 
+tags: [Intermediate Python Programming]
+excerpt: Intermediate Python Programming 
+---
+
+
+```python
+tensorboard  --logdir ~/ch3_tb_log --port 6006
+```
