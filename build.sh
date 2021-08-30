fa0077ecdb1a814f7708ea0b6049b7e4fe7aef9d
Seri Lee <sally20921@snu.ac.kr>
2021-08-31 00:32:06 +0900

    adding _site

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+brew install libxml2 libxslt
+bundle config path 'vendor/bundle' --local
+#bundle config build.nokogiri --use-system-libraries=true --with-xml2-include="$(xcrun --show-sdk-path)"/usr/include/libxml2
+bundle install 
